a6ad3b44cbaef0551a6fbc0da54fafb78d8c959c
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-11-19 21:56:52 +0100

    Process seconds

--- spec/KataSpec.js
+++ spec/KataSpec.js
@@ -3,16 +3,31 @@ describe('Kata', function() {
   beforeEach(function() {
   });
 
-  it('should be able to test', function() {
+  it('should be able to test.', function() {
     expect(true).toBeTruthy();
   });
 
+  it('should throw error when negative values.', function() {
+    var test = function() {
+      return formatDuration(-1);
+    }
+    expect(test).toThrow();
+  });
+
   it('should return "now" when 0 seconds', function() {
-    expect(true).toBeFalsy('Not implemented test.')
+    expect(formatDuration(0)).toEqual('now','Should return "now".');
   });
 
   it('should return "1 second" when 1', function() {
-    expect(true).toBeFalsy('Not implemented test.')
+    expect(formatDuration(1)).toEqual('1 second','Should return "1 second".');
+  });
+
+  it('should return "59 seconds" when 59', function() {
+    expect(formatDuration(59)).toEqual('59 seconds','Should return "59 seconds".');
+  });
+
+  it('should return "1 minute" when 60', function() {
+    expect(formatDuration(60)).toEqual('1 minute','Should return "1 minute".');
   });
 
   it('should return "1 minute and 2 seconds" when 62', function() {
--- src/Kata.js
+++ src/Kata.js
@@ -1,3 +1,20 @@
 function formatDuration(seconds) {
-  
+  var sec = 0,
+      result;
+
+    if (seconds < 0 ) throw {
+      name: "Negative value",
+      message: "Not able to use negative values."
+    }
+    if (seconds === 0) return 'now';
+
+    sec = seconds;
+    if (sec > 0) {
+      result = seconds + " second";
+      if (seconds > 1) {
+          result += 's';
+      }
+    }
+
+    return result;
 }
