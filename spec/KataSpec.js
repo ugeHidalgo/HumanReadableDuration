4d61bb53fdb6f438d1c25c011cd088ffb39814c6
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-11-19 20:55:28 +0100

    Initial scenario.

---
+++ spec/KataSpec.js
@@ -0,0 +1,33 @@
+describe('Kata', function() {
+  
+  beforeEach(function() {
+  });
+
+  it('should be able to test', function() {
+    expect(true).toBeTruthy();
+  });
+
+  it('should return "now" when 0 seconds', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+
+  it('should return "1 second" when 1', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+
+  it('should return "1 minute and 2 seconds" when 62', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+
+  it('should return "2 minutes" when 120', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+
+  it('should return "1 hour" when 3600', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+
+  it('should return "1 hour, 1 minute and 2 seconds" when 3662', function() {
+    expect(true).toBeFalsy('Not implemented test.')
+  });
+});
